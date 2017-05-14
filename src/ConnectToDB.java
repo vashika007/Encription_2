feb271e2279c839063894f26f55372ce8e2e0b27
shanmaster <shreemate@gmail.com>
2017-05-14 14:15:22 -0700

    adding hostname of the sql server

--- src/ConnectToDB.java
+++ src/ConnectToDB.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectToDB {
 
@@ -8,17 +9,31 @@ public class ConnectToDB {
 	public static Connection getConnection() {
 		if (conn != null) {
 			try {
-//				Context initContext = new InitialContext();
-//
-//				Context envContext = (Context) initContext.lookup("java:/comp/env");
-//				DataSource ds = (DataSource) envContext.lookup("jdbc/mySQL");
-//				conn = ds.getConnection();
-				
+
+				String HostName = System.getenv("OPENSHIFT_EXTMYSQL_DB_HOST");
+				String HostPort = System.getenv("OPENSHIFT_EXTMYSQL_DB_PORT");
+				String DBName = System.getenv("OPENSHIFT_EXTMYSQL_DB_NAME");
+				String DBUser = System.getenv("OPENSHIFT_EXTMYSQL_DB_USERNAME");
+				String DBPassword = System.getenv("OPENSHIFT_EXTMYSQL_DB_PASSWORD");
+
+				Properties connectionProps = new Properties();
+				connectionProps.put("user", DBUser);
+				connectionProps.put("password", DBPassword);
+
+				String ConnectionURL = "jdbc:mysql://" + HostName + ":" + HostPort + "/" + DBName;
+
 				Class.forName("com.mysql.jdbc.Driver").newInstance();
-               conn = (Connection) DriverManager.getConnection("jdbc:mysql://mysql:3306/decentralize", "admin", "admin");
+
+				System.out.println(" Creating connecton with ConnectionURL " + ConnectionURL + " connectionProps "
+						+ connectionProps);
+
+				// conn = (Connection)
+				// DriverManager.getConnection("jdbc:mysql://mysql:3306/decentralize",
+				// "admin", "admin");
+				conn = (Connection) DriverManager.getConnection(ConnectionURL, connectionProps);
 
 			} catch (Exception e) {
-		
+
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
