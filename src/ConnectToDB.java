b7f441328ecfb2ad7f27b884beb0720b243a56d3
shanmaster <shreemate@gmail.com>
2017-05-14 20:23:33 -0700

    updating the SQL host name

--- src/ConnectToDB.java
+++ src/ConnectToDB.java
@@ -1,6 +1,5 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
 public class ConnectToDB {
 
@@ -10,27 +9,29 @@ public class ConnectToDB {
 		if (conn != null) {
 			try {
 
-				String HostName = System.getenv("OPENSHIFT_EXTMYSQL_DB_HOST");
-				String HostPort = System.getenv("OPENSHIFT_EXTMYSQL_DB_PORT");
-				String DBName = System.getenv("OPENSHIFT_EXTMYSQL_DB_NAME");
-				String DBUser = System.getenv("OPENSHIFT_EXTMYSQL_DB_USERNAME");
-				String DBPassword = System.getenv("OPENSHIFT_EXTMYSQL_DB_PASSWORD");
-
-				Properties connectionProps = new Properties();
-				connectionProps.put("user", DBUser);
-				connectionProps.put("password", DBPassword);
-
-				String ConnectionURL = "jdbc:mysql://" + HostName + ":" + HostPort + "/" + DBName;
-
+//				String HostName = System.getenv("OPENSHIFT_EXTMYSQL_DB_HOST");
+//				String HostPort = System.getenv("OPENSHIFT_EXTMYSQL_DB_PORT");
+//				String DBName = System.getenv("OPENSHIFT_EXTMYSQL_DB_NAME");
+//				String DBUser = System.getenv("OPENSHIFT_EXTMYSQL_DB_USERNAME");
+//				String DBPassword = System.getenv("OPENSHIFT_EXTMYSQL_DB_PASSWORD");
+//
+//				Properties connectionProps = new Properties();
+//				connectionProps.put("user", DBUser);
+//				connectionProps.put("password", DBPassword);
+//
+//				String ConnectionURL = "jdbc:mysql://" + HostName + ":" + HostPort + "/" + DBName;
+//
+//				
+//
+//				System.out.println(" Creating connecton with ConnectionURL " + ConnectionURL + " connectionProps "
+//						+ connectionProps);
 				Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-				System.out.println(" Creating connecton with ConnectionURL " + ConnectionURL + " connectionProps "
-						+ connectionProps);
-
-				// conn = (Connection)
-				// DriverManager.getConnection("jdbc:mysql://mysql:3306/decentralize",
-				// "admin", "admin");
-				conn = (Connection) DriverManager.getConnection(ConnectionURL, connectionProps);
+				 conn = (Connection)
+				 DriverManager.getConnection("jdbc:mysql://mysql.aes-encrytion.svc:3306/decentralize",
+				 "admin", "admin");
+				
+//				conn = (Connection) DriverManager.getConnection(ConnectionURL, connectionProps);
 
 			} catch (Exception e) {
 
