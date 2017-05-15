58a09a38e6bcca85aa1a602cddaf915c125fd624
shanmaster <shreemate@gmail.com>
2017-05-14 20:34:29 -0700

    updating the pom

--- src/ConnectToDB.java
+++ src/ConnectToDB.java
@@ -28,7 +28,7 @@ public class ConnectToDB {
 				Class.forName("com.mysql.jdbc.Driver").newInstance();
 
 				 conn = (Connection)
-				 DriverManager.getConnection("jdbc:mysql://mysql.aes-encrytion.svc:3306/decentralize",
+				 DriverManager.getConnection("jdbc:mysql://10.130.25.244:3306/decentralize",
 				 "admin", "admin");
 				
 //				conn = (Connection) DriverManager.getConnection(ConnectionURL, connectionProps);
